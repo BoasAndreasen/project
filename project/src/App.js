3a37d64325b12975c64b3ef01269765e625e553d
Boas Andreasen <boasandreasen@gmail.com>
2025-04-26 10:09:44 +0100

    Unit tests

--- project/src/App.js
+++ project/src/App.js
@@ -5,11 +5,11 @@ import Logo from "./resources/Logo.svg"
 import { Routes, Route, BrowserRouter } from "react-router-dom"
 import { useReducer } from "react";
 
-const initializeTimes = () => {
+export const initializeTimes = () => {
   return ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
 };
 
-const updateTimes = (state, action) => {
+export const updateTimes = (state, action) => {
   switch (action.selectedDate) {
     case "2025-04-25":
       return ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
--- project/src/App.test.js
+++ project/src/App.test.js
@@ -1,8 +1,25 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import BookingForm from "./components/BookingForm/BookingForm";
+import { initializeTimes, updateTimes } from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders static text in BookingForm", () => {
+  render(<BookingForm availableTimes={[]} dispatch={() => {}} />);
+  expect(screen.getByText("Choose date")).toBeInTheDocument();
+  expect(screen.getByText("Choose time")).toBeInTheDocument();
+  expect(screen.getByText("Number of guests")).toBeInTheDocument();
+  expect(screen.getByText("Occasion")).toBeInTheDocument();
+  expect(screen.getByText("Make Your reservation")).toBeInTheDocument();
+});
+
+
+test("initializeTimes returns the correct initial times", () => {
+  const expectedTimes = ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
+  expect(initializeTimes()).toEqual(expectedTimes);
+});
+
+
+test("updateTimes returns the same state when no matching action is provided", () => {
+  const initialState = ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
+  const action = { selectedDate: "2025-04-26" };
+  expect(updateTimes(initialState, action)).toEqual(initialState);
 });
